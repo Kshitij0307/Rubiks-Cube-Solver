2f5df0a91aa0aff0876ed138bd93e69f7e956979
Kshitij Gupta <138508009+Kshitij0307@users.noreply.github.com>
2025-01-07 19:54:13 +0530

    Update main.js

--- Frontend/three/main.js
+++ Frontend/three/main.js
@@ -1,4 +1,4 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.130.1/build/three.module.js';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.169.0/build/three.module.js';
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 // Add these at the very top of your file, before any other code
